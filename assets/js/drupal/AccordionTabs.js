ae3364936c299b0145edbac51e9d68c0a292dfa3
Ben McClure <ben.mcclure@gmail.com>
2017-01-01 21:25:57 -0600

    Make JS changes to both versions of file.

--- assets/js/drupal/AccordionTabs.js
+++ assets/js/drupal/AccordionTabs.js
@@ -7,7 +7,7 @@
                 $(this)
                     .children('.AccordionTabs-tabContainer').first()
                     .children('.AccordionTabs-tabHeader').addClass('is-active')
-                    .next().addClass('is-open').show();
+                    .next().addClass('is-open');
             });
 
             tabs.on('click', ' > .AccordionTabs-tabContainer > .AccordionTabs-tabHeader', function (event) {
@@ -17,8 +17,8 @@
                 if (!tabHeader.hasClass('is-active')) {
                     var accordionTabs = tabHeader.closest('.AccordionTabs');
 
-                    accordionTabs.find('.is-open').removeClass('is-open').hide();
-                    tabHeader.next().toggleClass('is-open').toggle();
+                    accordionTabs.find('.is-open').removeClass('is-open');
+                    tabHeader.next().toggleClass('is-open');
                     accordionTabs.find('.is-active').removeClass('is-active');
                     tabHeader.addClass('is-active');
                 }
